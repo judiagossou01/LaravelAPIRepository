eecca0304d815e02b9a0e6a6d1667890c4866138
thebabycode <58846803+judiagossou01@users.noreply.github.com>
2023-04-28 00:16:56 +0100

    Add files via upload

---
+++ config/digit.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'status' => true,
+    'message' => 'Welcome to Digitpay API',
+    'data' => [
+        'service' => 'Digitpay API',
+        'version' => '1.0',
+        'language' => app()->getLocale(),
+        'apikey' => 'e8e4b03d652fbfffbb22acd109818810',
+        'support' => 'contact@ujima.sarl'
+    ]
+];
